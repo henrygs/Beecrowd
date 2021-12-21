bad81721963043d3f064dc82dbe6f2a7371aa5c9
henrygs <henrique-91@hotmail.com>
2021-12-20 22:06:48 -0300

    Outra forma de se fazer, mas a platafor nao permite

--- 1000/src/main/kotlin/Main.kt
+++ 1000/src/main/kotlin/Main.kt
@@ -1,14 +1,18 @@
-import java.math.RoundingMode
-import java.text.DecimalFormat
 import java.util.*
+import kotlin.math.pow
 
 fun main(args: Array<String>) {
-    
+
+    // Tem essa forma de fazer, mas a plataforma não permite
     val scan = Scanner(System.`in`)
 
-    val a = scan.nextLine().trim().toDouble()
+    val raio = scan.nextLine().trim().toDouble()
+    // usando a expressão/função pow
+    val exp = raio.pow(2)
     val pi = 3.14159
-    val area = pi * (a*a)
+    val area = pi * exp
 
     println("A=%.4f".format(area))
 }
+
+
